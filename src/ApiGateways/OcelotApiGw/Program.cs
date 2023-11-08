9cdd612ec6a3b92fdbad1c144337f28f3b8a0d75
Vlad-George Irimia <vlad-george.irimia@adesso-romania.ro>
2023-11-08 17:17:42 +0200

    added Ocelot

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,21 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddOcelot().AddCacheManager(x => x.WithDictionaryHandle());
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
