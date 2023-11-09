bd5e42e840a9bfbf1094f40c1d9936ef4f4694fe
Vlad-George Irimia <vlad-george.irimia@adesso-romania.ro>
2023-11-09 13:15:55 +0200

    shopping aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly HttpClient _httpClient;
+
+    public OrderService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+    {
+        var response = await _httpClient.GetAsync($"api/v1/Order/{userName}");
+        return await response.ReadContentAs<List<OrderResponseModel>>();
+    }
+}
